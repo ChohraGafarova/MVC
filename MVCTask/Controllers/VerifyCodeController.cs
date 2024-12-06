07cd457329dfa86fd219286b10ff260376ae4240
ChohraGafarova <cohre.qafarova@gmail.com>
2024-12-06 19:12:21 +0400

    Add project files.

---
+++ MVCTask/Controllers/VerifyCodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTask.Controllers
+{
+    public class VerifyCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
